dba5f3a3bd518e7762f8ca60aa137a174f288597
Timothy <timsu100@gmail.com>
2023-08-01 22:11:37 -0700

    basic files

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+--Business database
+DROP DATABASE IF EXISTS business_db;
+
+CREATE DATABASE inventory_db;
+
+USE  inventory_db;
+
+--department table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+--role table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+--employee table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manger_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manger_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
